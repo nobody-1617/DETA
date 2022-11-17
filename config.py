2c52ffb1502a454cde11e7411ed068686690b41e
nobody-1617 <117986052+nobody-1617@users.noreply.github.com>
2022-11-17 15:54:13 +0800

    commit

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ parser.add_argument('--data.test', type=str, default='cu_birds', metavar='TESTSE
 parser.add_argument('--data.num_workers', type=int, default=32, metavar='NEPOCHS',
                     help="Number of workers that pre-process images in parallel")
 
-# model args
+# model args (TSA)
 default_model_name = 'noname'
-parser.add_argument('--model.name', type=str, default="moco", metavar='MODELNAME',
+parser.add_argument('--model.name', type=str, default="url", metavar='MODELNAME',
                     help="A name you give to the extractor".format(default_model_name))
 parser.add_argument('--model.backbone', default='resnet18', help="Use ResNet18 for experiments (default: False)")
 parser.add_argument('--model.classifier', type=str, default='linear', choices=['none', 'linear', 'cosine'], help="Do classification using cosine similatity between activations and weights")
@@ -23,51 +23,6 @@ parser.add_argument('--model.pretrained', action='store_true', help="Using pretr
 # adaptor args
 parser.add_argument('--adaptor.opt', type=str, default='linear', help="type of adaptor, linear or nonlinear")
 
-# train args
-parser.add_argument('--train.batch_size', type=int, default=16, metavar='BS',
-                    help='number of images in a batch')
-parser.add_argument('--train.max_iter', type=int, default=500000, metavar='NEPOCHS',
-                    help='number of epochs to train (default: 10000)')
-parser.add_argument('--train.weight_decay', type=float, default=7e-4, metavar='WD',
-                    help="weight decay coef")
-parser.add_argument('--train.optimizer', type=str, default='momentum', metavar='OPTIM',
-                    help='optimization method (default: momentum)')
-parser.add_argument('--train.learning_rate', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.0001)')
-parser.add_argument('--train.sigma', type=float, default=1, metavar='SIGMA',
-                    help='weight of CKA loss on features')
-parser.add_argument('--train.beta', type=float, default=1, metavar='BETA',
-                    help='weight of KL-divergence loss on logits')
-parser.add_argument('--train.lr_policy', type=str, default='cosine', metavar='LR_policy',
-                    help='learning rate decay policy')
-parser.add_argument('--train.lr_decay_step_gamma', type=int, default=1e-1, metavar='DECAY_GAMMA',
-                    help='the value to divide learning rate by when decayin lr')
-parser.add_argument('--train.lr_decay_step_freq', type=int, default=10000, metavar='DECAY_FREQ',
-                    help='the value to divide learning rate by when decayin lr')
-parser.add_argument('--train.exp_decay_final_lr', type=float, default=8e-5, metavar='FINAL_LR',
-                    help='the value to divide learning rate by when decayin lr')
-parser.add_argument('--train.exp_decay_start_iter', type=int, default=30000, metavar='START_ITER',
-                    help='the value to divide learning rate by when decayin lr')
-parser.add_argument('--train.cosine_anneal_freq', type=int, default=4000, metavar='ANNEAL_FREQ',
-                    help='the value to divide learning rate by when decayin lr')
-parser.add_argument('--train.nesterov_momentum', action='store_true', help="If to augment query images in order to avearge the embeddings")
-
-# evaluation during training
-parser.add_argument('--train.eval_freq', type=int, default=5000, metavar='EVAL_FREQ',
-                    help='How often to evaluate model during training')
-parser.add_argument('--train.eval_size', type=int, default=300, metavar='EVAL_SIZE',
-                    help='How many episodes to sample for validation')
-parser.add_argument('--train.resume', type=int, default=1, metavar='RESUME_TRAIN',
-                    help="Resume training starting from the last checkpoint (default: True)")
-
-
-# creating a database of features
-parser.add_argument('--dump.name', type=str, default='', metavar='DUMP_NAME',
-                    help='Name for dumped dataset of features')
-parser.add_argument('--dump.mode', type=str, default='test', metavar='DUMP_MODE',
-                    help='What split of the original dataset to dump')
-parser.add_argument('--dump.size', type=int, default=600, metavar='DUMP_SIZE',
-                    help='Howe many episodes to dump')
 
 # test args
 parser.add_argument('--test.size', type=int, default=600, metavar='TEST_SIZE',
@@ -112,6 +67,8 @@ parser.add_argument('--n_regions', type=int, default=2, metavar='N_Regions',
 parser.add_argument('--ratio', type=float, default=0., metavar='Ratio',
                     help='ratio of support samples with data bias')
 parser.add_argument('--ours', action='store_true', help="ours or baseline")
+parser.add_argument('--pretrained_model', type=str, default="MoCo", metavar='MODELNAME',
+                    help="a model to be adapted")
 
 
 
